a2eb9108b235d75237f5c50f458a807ceb88c42d
ZeinaAyman <56726381+ZeinaAyman@users.noreply.github.com>
2021-05-04 20:37:08 +0200

    login

---
+++ Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="master.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="topnav">
+      <h2>Designs Solutions</h2>
+      <h3>LOGO</h3>
+    </div>
+
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</p>
+
+      <div class="container">
+        <h1>Login</h1>
+        <form action= "" method="post">
+        	Name:<br>
+        	<input type="text" name="name">  <br>
+        	Password:<br>
+        	<input type="password" name="password"><br>
+        	<input type="submit" value="Submit" name="Submit">
+        	<input type="reset">
+        </form>
+      </div>
+  </body>
+</html>
