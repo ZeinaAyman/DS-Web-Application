87ffa4c23b1a3ccef349239649fcd179651ad518
ZeinaAyman <56726381+ZeinaAyman@users.noreply.github.com>
2021-05-08 05:37:25 +0200

    Login with CSS changes

--- Login.php
+++ Login.php
@@ -16,12 +16,43 @@
       <div class="container">
         <h1>Sign In</h1>
         <form action= "" method="post">
-        	Name:<br>
-        	<input type="text" name="name">  <br>
-        	Password:<br>
-        	<input type="password" name="password"><br>
-        	<input type="submit" value="Submit" name="Submit">
+          <div class="input">
+            <input class="info" type="text" placeholder="Email"  name="name">
+          </div>
+          <div class="input">
+            <input class="info" type="password" placeholder="Password" name="password"><br>
+          </div>
+        	<input class="button" type="submit" value="Log in" name="Submit">
         </form>
       </div>
   </body>
+  <script type="text/javascript">
+  var inputs = document.querySelectorAll('.input input');
+  for (let i = 0; i < inputs.length; i++) {
+    var placeholder = inputs[i].getAttribute('placeholder');
+    var newPlaceholder = document.createElement('div');
+    newPlaceholder.setAttribute('id', 'input' + i);
+    newPlaceholder.classList.add('placeholder');
+    newPlaceholder.innerHTML = placeholder;
+    inputs[i].setAttribute('placeholder', '');
+    inputs[i].parentElement.appendChild(newPlaceholder);
+    placeholder = document.getElementById('input' + i);
+    if (inputs[i].value.length > 0) {
+      placeholder.classList.add('selected');
+    } else {
+      placeholder.classList.remove('selected');
+    }
+
+    inputs[i].addEventListener('input', function() {
+      var placeholder = document.getElementById('input' + i);
+      if (this.value.length > 0) {
+        placeholder.classList.add('selected');
+        this.parentElement.classList.add('selected');
+      } else {
+        placeholder.classList.remove('selected');
+        this.parentElement.classList.remove('selected');
+      }
+    });
+  }
+  </script>
 </html>
