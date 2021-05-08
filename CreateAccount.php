574ed671c5805448881988a2d3c29b0c47b935ef
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-05-08 05:28:49 +0200

    Admin Create Account

---
+++ CreateAccount.php
@@ -0,0 +1,78 @@
+<style media="screen">
+  body
+  {
+    margin: 0;
+    background-color: #404B69;
+  }
+  .header
+  {
+      width: 100%;
+      height: 20%;
+      background: rgba(40,49,73,1);
+
+  }
+  .title
+  {
+
+
+    color: #eeebd4;
+    font-family: Comic Sans MS;
+    font-weight: "Bold";
+    font-size: "36px";
+    opacity: 1
+
+
+
+  }
+  .create-acc
+  {
+   margin-left: 42%;
+    color: E3B7A5;
+    font-size: 36;
+    font-family: : "Montserrat";
+    opacity: 1;
+  }
+  .container
+  {
+  position: fixed;
+  top: 30%;
+  left: 35%;
+  width: 30%;
+  height: 60%;
+  border-width: 2px;
+  border-color: #E3B7A5;
+  border-style: solid;
+  }
+
+ .empname
+ {
+   margin-top: 2.5%;
+   margin-bottom: 2.5%;
+   margin-left: 3.5%;
+   color : white;
+   font-size: 24px;
+   display: block;
+ }
+ .empinput
+ {
+  margin-left: 3.5%;
+  background-color: #404B69;
+  border-color: black;
+  width: 93%;
+ }
+</style>
+<div class="header">
+<p class="title">Design Solutions</p>
+
+</div>
+
+<div class="create-acc">
+  Create Account
+</div>
+
+  <div class="container">
+    <label class="empname">Employee Name</label>
+
+
+  </div>
+</div>
