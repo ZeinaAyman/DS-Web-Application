6a59992fa659d5fe7c75f8eb8022eeb9cfb5402a
jj1005 <59148313+jj1005@users.noreply.github.com>
2021-05-08 21:12:09 +0200

    gallery frames added
    code is generated from figma to html generator plugin
    
    also added their styles.css

---
+++ gallery2.php
@@ -0,0 +1,28 @@
+<html lang="en">
+          <head>
+            <meta charset="utf-8">
+          
+            <title>Gallery</title>
+            <meta name="description" content="Figma htmlGenerator">
+            <meta name="author" content="htmlGenerator">
+            <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Mplus+1p Bold&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+
+            <link rel="stylesheet" href="styles1.css">
+            
+            <style>
+              /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+              body {background: #E5E5E5; }
+            </style>
+          
+          </head>
+          
+          <body>
+            <div class=e338_1412><div class=e338_1413><div  class="e338_1414"></div></div><div class=e338_1415><div  class="e338_1416"></div></div><div class=e338_1417><div  class="e338_1418"></div></div><div class=e338_1419><div  class="e338_1420"></div></div><div class=e338_1421><div  class="e338_1422"></div></div><div class=e338_1423><div  class="e338_1424"></div></div><span  class="e338_1425">Project Details</span><span  class="e338_1426">
+</span><div  class="e338_1427"></div><div class=e338_1428><div  class="e338_1429"></div></div><span  class="e338_1430">Sed ut perspiciatis unde omnis</span><span  class="e338_1431">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</span><div  class="e338_1432"></div><div class=e338_1433><div  class="ei338_1433_338_1429"></div></div><span  class="e338_1434">Sed ut perspiciatis unde omnis</span><span  class="e338_1435">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</span><div class=e338_1436><div class=e338_1437><span  class="e338_1438">Logo</span><div class=e338_1439><div  class="e338_1440"></div><div  class="e338_1441"></div></div><div class=e338_1442><div  class="e338_1443"></div><div  class="e338_1444"></div></div></div><span  class="e338_1445">Home</span><span  class="e338_1446">Gallery</span><span  class="e338_1447">About Us</span><span  class="e338_1448">Contact</span></div><div  class="e338_1449"></div><span  class="e338_1450">© Copyright 2021 - Desig-Solutions</span><div class=e338_1451><div  class="ei338_1451_338_1617"></div></div><div class=e338_1452><div  class="ei338_1452_338_1619"></div></div></div>
+          </body>
+          </html>
