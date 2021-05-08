9d6be3ae4698d7a23c71883eecf73c9a391a50c7
ZeinaAyman <56726381+ZeinaAyman@users.noreply.github.com>
2021-05-08 05:56:32 +0200

    Admin perspective

---
+++ Admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="master.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="topnav">
+      <h2>Designs Solutions</h2>
+      <h3>LOGO</h3>
+    </div>
+
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</p>
+
+      <div class="container">
+        <h1>Welcome</h1>
+
+        	<input class="buttonx" type="submit" value="Create Account" name="Submit"><br>
+
+          <input class="buttonx" type="submit" value="Manage Accounts" name="Submit">
+      </div>
+  </body>
+</html>
