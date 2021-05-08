c1cb15142059d1ced041a7efa9d554ddb4cbf03f
ZeinaAyman <56726381+ZeinaAyman@users.noreply.github.com>
2021-05-08 06:42:58 +0200

    Navbar Added

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="master.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="navbar">
+      <h2>Designs Solutions</h2>
+      <div class="links">
+        <a href="#">Log Out</a>
+        <a href="#">Profile</a>
+        <a href="#">Projects</a>
+        <a href="#">Home</a>
+      </div>
+    </div>
+  </body>
+</html>
