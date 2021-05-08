03e79367363ea25905ab3a470c597fbe330d1638
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2021-05-08 09:05:55 +0200

    commit

---
+++ myprojects.php
@@ -0,0 +1,54 @@
+<?php include 'navbar.php' ?>
+<link rel="stylesheet" href="Project.css">
+<body>
+  <h1 class="title">All Projects</h1>
+  <div class="add-p">
+    Create New Project
+    <img src="images/add.png" alt="" class="add-p-icon">
+  </div>
+  <div class="project">
+    <div class="p-img">
+
+    </div>
+    <div class="p-info">
+      <h1>Project 1</h1><br>
+      <h3>Client 1 FullName</h3><br>
+      <h3>Designer 1 FullName</h3><br>
+      <h3>Designer 1 ID</h3>
+    </div>
+    <div class="p-status">
+      <h3>Status:<span class="status-orange">Ongoing</span></h3>
+    </div>
+  </div>
+
+  <div class="project">
+    <div class="p-img">
+
+    </div>
+    <div class="p-info">
+      <h1>Project 1</h1><br>
+      <h3>Client 1 FullName</h3><br>
+      <h3>Designer 1 FullName</h3><br>
+      <h3>Designer 1 ID</h3>
+    </div>
+    <div class="p-status">
+      <h3>Status:<span class="status-green">Finished</span></h3>
+    </div>
+  </div>
+
+  <div class="project">
+    <div class="p-img">
+
+    </div>
+    <div class="p-info">
+      <h1>Project 1</h1><br>
+      <h3>Client 1 FullName</h3><br>
+      <h3>Designer 1 FullName</h3><br>
+      <h3>Designer 1 ID</h3>
+    </div>
+    <div class="p-status">
+      <h3>Status:<span class="status-red">Dropped</span></h3>
+    </div>
+  </div>
+
+</body>
