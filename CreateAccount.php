c0dada39a38f180432399ab89780aee400c076b0
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-05-08 06:37:30 +0200

    CSS changes

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,87 +1,23 @@
+<link rel="stylesheet" href="master.css">
 <style media="screen">
   body
   {
     margin: 0;
     background-color: #404B69;
   }
-  .header
-  {
-      width: 100%;
-      height: 20%;
-      background: rgba(40,49,73,1);
 
-  }
-  .title
-  {
-
-
-    color: #eeebd4;
-    font-family: Comic Sans MS;
-    font-weight: "Bold";
-    font-size: "36px";
-    opacity: 1
-
-
-
-  }
-  .create-acc
-  {
-   margin-left: 42%;
-    color: E3B7A5;
-    font-size: 36;
-    font-family: : "Montserrat";
-    opacity: 1;
-  }
-  .container
-  {
-  position: fixed;
-  top: 30%;
-  left: 35%;
-  width: 30%;
-  height: 60%;
-  border-width: 2px;
-  border-color: #E3B7A5;
-  border-style: solid;
-  }
-
- .empname
- {
-   margin-top: 2.5%;
-   margin-bottom: 2.5%;
-   margin-left: 3.5%;
-   color : white;
-   font-size: 24px;
-   display: block;
- }
- .empinput,.emplist
- {
-  margin-left: 3.5%;
-  background-color: #404B69;
-  border-color: black;
-  width: 93%;
- }
- .createbut
- {
-   width: 20%;
-   position: absolute;
-   left: 40%;
-   top: 80%;
-   border-radius: 6%;
-   height: 10%;
-   background-color: #E3B7A5;
- }
 
 </style>
-<div class="header">
-<p class="title">Design Solutions</p>
-
+<div class="topnav">
+  <h2>Designs Solutions</h2>
+  <h3>LOGO</h3>
 </div>
 
 <div class="create-acc">
   Create Account
 </div>
 
-  <div class="container">
+  <div class="createacccontainer">
     <form class="" action="index.html" method="post">
       <label class="empname">Employee Name</label>
 
