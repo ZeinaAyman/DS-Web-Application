fc0356e367485dec888ae0e9cf752c41d0f8d0bb
jj1005 <59148313+jj1005@users.noreply.github.com>
2021-05-19 20:43:54 +0200

    gallery navbar added
    About us added the new navbar

---
+++ galleryNavbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="master.css">
+    <title></title>
+</head>
+
+<body>
+    <div class="navbar">
+        <h2>Designs Solutions</h2>
+        <div class="links">
+            <a href="Home.php">Home</a>
+            <a href="gallery1.php">Galley</a>
+            <a href="AboutUs.php">About Us</a>
+            <a href="contactUs.php">Contact</a>
+        </div>
+    </div>
+</body>
+
+</html>
