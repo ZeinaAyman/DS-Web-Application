ce4b98750d6163c533e1920bac1f0a8b2a63375f
jj1005 <59148313+jj1005@users.noreply.github.com>
2021-05-22 19:43:23 +0200

    fixed some screens

--- gallery1.php
+++ gallery1.php
@@ -1,26 +1,74 @@
+<?php
+include 'galleryNavbar.php';
+?>
 <html lang="en">
-          <head>
-            <meta charset="utf-8">
-          
-            <title>Gallery</title>
-            <meta name="description" content="Figma htmlGenerator">
-            <meta name="author" content="htmlGenerator">
-            <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
 
-            <link rel="stylesheet" href="styles1.css">
-            
-            <style>
-              /*
+<head>
+  <meta charset="utf-8">
+
+  <title>Gallery</title>
+  <meta name="description" content="Figma htmlGenerator">
+  <meta name="author" content="htmlGenerator">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="styles1.css">
+
+  <style>
+    /*
                 Figma Background for illustrative/preview purposes only.
                 You can remove this style tag with no consequence
               */
-              body {background: #E5E5E5; }
-            </style>
-          
-          </head>
-          
-          <body>
-            <div class=e338_1374><div class=e338_1375><div class=e338_1376><div  class="e338_1377"></div></div><div class=e338_1378><div  class="e338_1379"></div></div><div class=e338_1380><div  class="e338_1381"></div></div><div class=e338_1382><div  class="e338_1383"></div></div><div class=e338_1384><div  class="e338_1385"></div></div><div class=e338_1386><div  class="e338_1387"></div></div><div class=e338_1388><div  class="e338_1389"></div></div><div class=e338_1390><div  class="e338_1391"></div></div><div class=e338_1392><span  class="e338_1393">Company Projects</span><span  class="e338_1394">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</span></div></div><div class=e338_1395><div class=e338_1396><span  class="e338_1397">Logo</span><div class=e338_1398><div  class="e338_1399"></div><div  class="e338_1400"></div></div><div class=e338_1401><div  class="e338_1402"></div><div  class="e338_1403"></div></div></div><span  class="e338_1404">Home</span><span  class="e338_1405">Gallery</span><span  class="e338_1406">About Us</span><span  class="e338_1407">Contact</span></div><div  class="e338_1408"></div><span  class="e338_1409">© Copyright 2021 - Desig-Solutions</span><div class=e338_1410><div  class="ei338_1410_338_1617"></div></div><div class=e338_1411><div  class="ei338_1411_338_1619"></div></div></div>
-          </body>
-          </html>
+    body {
+      background: #E5E5E5;
+    }
+  </style>
+
+</head>
+
+<body>
+  <div class=e338_1374>
+    <div class=e338_1375>
+      <div class=e338_1376>
+        <div class="e338_1377">
+          <img></img>
+        </div>
+      </div>
+      <div class=e338_1378>
+        <div class="e338_1379"></div>
+      </div>
+      <div class=e338_1380>
+        <div class="e338_1381"></div>
+      </div>
+      <div class=e338_1382>
+        <div class="e338_1383"></div>
+      </div>
+      <div class=e338_1384>
+        <div class="e338_1385"></div>
+      </div>
+      <div class=e338_1386>
+        <div class="e338_1387"></div>
+      </div>
+      <div class=e338_1388>
+        <div class="e338_1389"></div>
+      </div>
+      <div class=e338_1390>
+        <div class="e338_1391"></div>
+      </div>
+      <div class=e338_1392>
+        <span class="e338_1393">Company Projects</span>
+        <span class="e338_1394">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</span>
+      </div>
+    </div>
+    <div class="e338_1408"></div>
+    <span class="e338_1409">© Copyright 2021 - Desig-Solutions</span>
+    <div class=e338_1410>
+      <div class="ei338_1410_338_1617"></div>
+    </div>
+    <div class=e338_1411>
+      <div class="ei338_1411_338_1619"></div>
+    </div>
+  </div>
+</body>
+
+</html>
--- galleryNavbar.php
+++ galleryNavbar.php
@@ -1,22 +1,38 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="master.css">
-    <title></title>
+
+
+    <style>
+        .navbar {
+            overflow: hidden;
+            background-color: #76323F;
+            font-size: 17px;
+            padding: 20px 16px;
+        }
+
+        .navbar .links a {
+            float: right;
+            text-decoration: none;
+            font-family: Arial;
+            color: #B1DBAD;
+            padding-left: 35px;
+        }
+
+        h2 {
+            position: absolute;
+            font-family: sans-serif;
+            color: #EDEAD3;
+            padding: 0px 50px;
+        }
+    </style>
 </head>
 
-<body>
-    <div class="navbar">
-        <h2>Designs Solutions</h2>
-        <div class="links">
-            <a href="Home.php">Home</a>
-            <a href="gallery1.php">Galley</a>
-            <a href="AboutUs.php">About Us</a>
-            <a href="contactUs.php">Contact</a>
-        </div>
-    </div>
-</body>
 
-</html>
+<div class="navbar">
+    <h2>Designs Solutions</h2>
+    <div class="links">
+        <a href="contactUs.php">Contact</a>
+        <a href="AboutUs.php">About Us</a>
+        <a href="gallery1.php">Gallery</a>
+        <a href="Home.php">Home</a>
+    </div>
+</div>
