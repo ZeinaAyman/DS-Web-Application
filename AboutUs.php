4f199ac72293f95e9efbcb65e273a3165f71b544
zeina <zeina.ayman@hotmail.com>
2021-06-06 20:46:14 +0200

    Merge branch 'master' of https://github.com/ZeinaAyman/DS-Web-Application

--- AboutUs.php
+++ AboutUs.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php include "Homenavbar.php"; ?>
 <style media="screen">
 .338_1453 {
@@ -239,15 +240,293 @@ text-align:center;
 font-size:15px;
 letter-spacing:-1.5;
 }
+=======
+<?php
+include 'galleryNavbar.php';
+?>
+<link rel="stylesheet" href="master.css">
+<style media="screen">
+  .338_1453 {
+    overflow: hidden;
+  }
+>>>>>>> ce4b98750d6163c533e1920bac1f0a8b2a63375f
 
+  .e338_1453 {
+    background-color: rgba(132.0000073313713, 127.00000002980232, 121.00000038743019, 1);
+    width: 1440px;
+    height: 1024px;
+    position: absolute;
+  }
+
+  .e338_1454 {
+    width: 1440px;
+    height: 572px;
+    position: absolute;
+    left: 0px;
+    top: 60px;
+  }
+
+  .ei338_1454_338_1429 {
+    background-color: rgba(229.00000154972076, 229.00000154972076, 229.00000154972076, 1);
+    width: 1440px;
+    height: 572px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  }
+
+  .i338_1454_338_1429 {
+    border: 1px solid rgba(0, 0, 0, 1);
+  }
+
+  .e338_1455 {
+    color: rgba(214.00000244379044, 232.00000137090683, 233.00000131130219, 1);
+    width: 903px;
+    height: 46px;
+    position: absolute;
+    left: 202px;
+    top: 668px;
+    font-family: Montserrat;
+    text-align: left;
+    font-size: 24px;
+    letter-spacing: -1.5;
+    line-height: px;
+  }
+
+  .e338_1456 {
+    color: rgba(214.00000244379044, 232.00000137090683, 233.00000131130219, 1);
+    width: 903px;
+    height: 153px;
+    position: absolute;
+    left: 202px;
+    top: 714px;
+    font-family: Montserrat;
+    text-align: left;
+    font-size: 18px;
+    letter-spacing: -1.5;
+    line-height: px;
+  }
+
+  .e338_1457 {
+    color: rgba(214.00000244379044, 232.00000137090683, 233.00000131130219, 1);
+    width: 903px;
+    height: 46px;
+    position: absolute;
+    left: 202px;
+    top: 826px;
+    font-family: Montserrat;
+    text-align: left;
+    font-size: 24px;
+    letter-spacing: -1.5;
+    line-height: px;
+  }
+
+  .e338_1458 {
+    color: rgba(214.00000244379044, 232.00000137090683, 233.00000131130219, 1);
+    width: 903px;
+    height: 153px;
+    position: absolute;
+    left: 201px;
+    top: 872px;
+    font-family: Montserrat;
+    text-align: left;
+    font-size: 18px;
+    letter-spacing: -1.5;
+    line-height: px;
+  }
+
+  .e338_1459 {
+    width: 393px;
+    height: 85px;
+    position: absolute;
+    left: 523px;
+    top: 261px;
+  }
+
+  .e338_1460 {
+    opacity: 0.30000001192092896;
+    width: 393px;
+    height: 85px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  }
+
+  .338_1460 {
+    border: 1px solid rgba(196.00000351667404, 196.00000351667404, 196.00000351667404, 1);
+  }
+
+  .e338_1461 {
+    color: rgba(227.00000166893005, 183.00000429153442, 165.00000536441803, 1);
+    width: 356px;
+    height: 67px;
+    position: absolute;
+    left: 0px;
+    top: 9px;
+    font-family: Montserrat;
+    text-align: center;
+    font-size: 48px;
+    letter-spacing: -1.5;
+    line-height: px;
+  }
+
+  .e338_1462 {
+
+    background-color: rgba(229.00000154972076, 229.00000154972076, 229.00000154972076, 1);
+    width: 1440px;
+    height: 60px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  }
+
+  .e338_1463 {
+    background-color: rgba(40.00000141561031, 49.00000087916851, 73.00000324845314, 1);
+    width: 122.07408905029297px;
+    height: 32px;
+    position: absolute;
+    left: 80px;
+    top: 14px;
+  }
+
+  .e338_1464 {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    color: rgba(0, 0, 0, 1);
+    width: 87.70370483398438px;
+    height: 32px;
+    position: absolute;
+    left: 34.37038040161133px;
+    top: 0px;
+    font-family: Comfortaa;
+    text-align: center;
+    font-size: 28.444446563720703px;
+    letter-spacing: -1.5;
+  }
+
+  .e338_1465 {
+    background-color: rgba(204.00000303983688, 204.00000303983688, 204.00000303983688, 1);
+    width: 22.518518447875977px;
+    height: 22.518518447875977px;
+    position: absolute;
+    left: 0.000008098707439785358px;
+    top: 2.962960720062256px;
+  }
+
+  .e338_1466 {
+    background-color: rgba(196.00000351667404, 196.00000351667404, 196.00000351667404, 1);
+    width: 22.518518447875977px;
+    height: 6.672153472900391px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  }
+
+  .e338_1467 {
+    transform: rotate(-90deg);
+    background-color: rgba(196.00000351667404, 196.00000351667404, 196.00000351667404, 1);
+    width: 22.518518447875977px;
+    height: 6.672153472900391px;
+    position: absolute;
+    left: 6.672144889831543px;
+    top: 22.518522262573242px;
+  }
+
+  .e338_1468 {
+    transform: rotate(-180deg);
+    background-color: rgba(0, 0, 0, 1);
+    width: 22.518518447875977px;
+    height: 22.518518447875977px;
+    position: absolute;
+    left: 22.51852035522461px;
+    top: 25.481481552124023px;
+  }
+
+  .e338_1469 {
+    transform: rotate(-180deg);
+    background-color: rgba(196.00000351667404, 196.00000351667404, 196.00000351667404, 1);
+    width: 22.518518447875977px;
+    height: 6.672153472900391px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  }
+
+  .e338_1470 {
+    transform: rotate(90.00000000000003deg);
+    background-color: rgba(196.00000351667404, 196.00000351667404, 196.00000351667404, 1);
+    width: 22.518518447875977px;
+    height: 6.672153472900391px;
+    position: absolute;
+    left: -6.672139644622799px;
+    top: -22.518522262573242px;
+  }
+
+  .e338_1471 {
+    color: rgba(234.00000125169754, 232.00000137090683, 218.00000220537186, 1);
+    width: 47px;
+    height: 18px;
+    position: absolute;
+    left: 1046px;
+    top: 21px;
+    font-family: Montserrat;
+    text-align: center;
+    font-size: 15px;
+    letter-spacing: -1.5;
+  }
+
+  .e338_1472 {
+    color: rgba(234.00000125169754, 232.00000137090683, 218.00000220537186, 1);
+    width: 52px;
+    height: 18px;
+    position: absolute;
+    left: 1132px;
+    top: 21px;
+    font-family: Montserrat;
+    text-align: center;
+    font-size: 15px;
+    letter-spacing: -1.5;
+  }
+
+  .e338_1473 {
+    color: rgba(234.00000125169754, 232.00000137090683, 218.00000220537186, 1);
+    width: 69px;
+    height: 18px;
+    position: absolute;
+    left: 1215px;
+    top: 21px;
+    font-family: Montserrat;
+    text-align: center;
+    font-size: 15px;
+    letter-spacing: -1.5;
+  }
+
+  .e338_1474 {
+    color: rgba(234.00000125169754, 232.00000137090683, 218.00000220537186, 1);
+    width: 59px;
+    height: 18px;
+    position: absolute;
+    left: 1315px;
+    top: 21px;
+    font-family: Montserrat;
+    text-align: center;
+    font-size: 15px;
+    letter-spacing: -1.5;
+  }
 </style>
 
 <div class=e338_1453>
   <div class=e338_1454>
     <div class="ei338_1454_338_1429"></div>
-  </div><span  class="e338_1455">Our Vision</span><span  class="e338_1456">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</span><span  class="e338_1457">Our Mission</span><span  class="e338_1458">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</span>
+  </div><span class="e338_1455">Our Vision</span><span class="e338_1456">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</span><span class="e338_1457">Our Mission</span><span class="e338_1458">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</span>
   <div class=e338_1459>
-    <div class="e338_1460"></div><span  class="e338_1461">About Us</span>
+    <div class="e338_1460"></div><span class="e338_1461">About Us</span>
   </div>
+<<<<<<< HEAD
 
 </div>
+=======
+  <div class=e338_1462>
+
+  </div>
+</div>
+>>>>>>> ce4b98750d6163c533e1920bac1f0a8b2a63375f
--- photos.php
+++ photos.php
@@ -1,1 +1,76 @@
-<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet" /><link href="./css/main.css" rel="stylesheet" /><title>Document</title></head><body><div class="v338_1041"><div class="v338_1042"></div><div class="v338_1043"></div><div class="v338_1044"></div><div class="v338_1045"></div><div class="v338_1046"></div><div class="v338_1047"></div><div class="v338_1048"></div><div class="v338_1049"></div><div class="v338_1050"></div><span class="v338_1051">Project Photos</span><span class="v338_1052">Add More Photos</span><div class="v338_1053"></div><div class="v338_1054"></div><div class="v338_1055"><div class="v338_1056"><span class="v338_1057">Logo</span><div class="name"></div><div class="name"></div></div><span class="v338_1064">Home</span><span class="v338_1065">Gallery</span><span class="v338_1066">About Us</span><span class="v338_1067">Contact</span></div></div></body></html>
+<?php
+include 'galleryNavbar.php';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Document</title>
+    <style>
+        .gallery__img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="v338_1041">
+        <div class="v338_1042">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1043">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1044">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1045">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1046">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1047">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1048">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1049">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <div class="v338_1050">
+            <figure class="gallery__item ">
+                <img src="images/building.jpg" alt="Gallery image 6" class="gallery__img">
+            </figure>
+        </div>
+        <span class="v338_1051">Project Photos</span><span class="v338_1052">Add More Photos</span>
+        <div class="v338_1053"></div>
+        <div class="v338_1054"></div>
+
+    </div>
+</body>
+
+</html>
