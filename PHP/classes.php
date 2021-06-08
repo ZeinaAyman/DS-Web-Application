d653973c3ca849214089d4e3ea970c69bab6ee98
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2021-06-08 22:37:45 +0200

    added class admin, designer and functions : feed , creataccount

--- PHP/classes.php
+++ PHP/classes.php
@@ -2,14 +2,32 @@
 
   class User
   {
-    public $ID;
     public $Name;
     public $Email;
-    public $password;
-    public $type;
     function __construct()
     {
 
+    }
+
+  }
+
+
+  class Employee extends User
+  {
+    public $ID;
+    public $Password;
+    public $Type;
+    function __construct()
+    {
+
+    }
+
+    public function Feed($Name,$Email,$Password,$Type)
+    {
+      $this->Name=$Name;
+      $this->Email=$Email;
+      $this->Password=$Password;
+      $this->Type=$Type;
     }
 
     public function Login($DB,$email,$password)
@@ -20,6 +38,12 @@
         {
           echo "done";
           $DB->ok=0;
+          $row = mysqli_fetch_assoc($result);
+          $this->ID=$row['ID'];
+          $this->Name=$row['Name'];
+          $this->Email=$row['Email'];
+          $this->Password=$row['Password'];
+          $this->Type=$row['Type'];
           header("Location: Home.php");
         }
         else
@@ -27,11 +51,32 @@
           $msg='User not found';
         }
     }
-
   }
 
+  class Designer extends Employee
+  {
 
+    function __construct()
+    {
 
+    }
+  }
+
+  class Admin extends Employee
+  {
+
+    function __construct()
+    {
+
+    }
+
+    public function CreateAccount($DB,$EMP)
+    {
+      $DB->query = "INSERT into user (Name,Email,Type,Password) VALUES('".$EMP->Name."','".$EMP->Email."','".$EMP->Type."','".$EMP->Password."')";
+      $result = $DB->query();
+    }
+
+  }
 
 
  ?>
