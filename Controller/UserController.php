207a7a66af188c9117390a4b3ed1ea6997be7557
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2021-06-15 21:28:17 +0200

    function update user

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -22,5 +22,34 @@ include 'Controller.php';
 
          }
       }
+
+      public function C_UpdateUser()
+      {
+        $err = false;
+        if(isset($_POST['submit']))
+         {
+                 $name=$_POST['name'];
+                 $email=$_POST['email'];
+                 $password=$_POST['password'];
+                 $newpassword=$_POST['NewPassword'];
+                 $confirmpassword=$_POST['ConfirmPassword'];
+
+                 if(!preg_match('/[a-z0-9._%+-]+@+[a-z0-9._%+-]+.+[a-z0-9._%+-]/',$email)){
+                   $msg="Please enter a valid email";
+                   $err = true;
+                 }
+
+                 if ($newpassword!=$confirmpassword) {
+                   $msg="Password doesn't match";
+                   $err = true;
+                 }
+
+                 if(!$err){
+                   $this->Model->connect();
+                   $this->Model->UpdateUser($this->Model,$name,$email,$password);
+                 }
+
+         }
+      }
   }
  ?>
