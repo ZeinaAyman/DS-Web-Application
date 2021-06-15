e86ec3eee113c36955afd7ed157c76e5fcc5edc0
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2021-06-15 21:29:56 +0200

    profile udate form in viewer

--- View/ViewUser.php
+++ View/ViewUser.php
@@ -23,24 +23,24 @@ class ViewUser extends View
       $html="";
       $html.="<form class='profilecontainer' action='Profile.php' method='post'>";
       $html.="<label class='empinfo'>First Name</label>";
-      $html.="<input type='text' class='empinput' value=".unserialize($online)->Name.">";
+      $html.="<input type='text' name='name' class='empinput' value=".unserialize($online)->Name.">";
 
       $html.="<label class='empinfo'>Last Name</label>";
       $html.="<input type='text' class='empinput' value='Deo'>";
 
       $html.="<label class='empinfo'>Email</label>";
-      $html.="<input type='text' class='empinput' value=".unserialize($online)->Email.">";
+      $html.="<input type='text' name='email' class='empinput' value=".unserialize($online)->Email.">";
 
       $html.="<label class='empinfo'>Old Password</label>";
-      $html.="<input type='password' class='empinput' value=".unserialize($online)->Password.">";
+      $html.="<input type='password' name='password' class='empinput' value=".unserialize($online)->Password.">";
 
       $html.="<label class='empinfo'>New Password</label>";
-      $html.="<input type='password' class='empinput' value='' >";
+      $html.="<input type='password' name='NewPassword' class='empinput' value='' >";
 
       $html.="<label class='empinfo>Confirm Password</label>";
-      $html.="<input type='password' class='empinput' value='' >";
+      $html.="<input type='password' name='ConfirmPassword' class='empinput' value='' >";
       $html.="<br>";
-      $html.="<input type='submit' class='updatebut' value='Update Profile'>";
+      $html.="<input type='submit' name='submit' class='updatebut' value='Update Profile'>";
 
       $html.="</form>";
 
