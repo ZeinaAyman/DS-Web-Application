51a49f0ef84c4439f2c71bd74d3f39eea5ce6703
Abdelrhman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-06-18 17:51:50 +0200

    Added my projects section of home page

--- View/ViewDesigner.php
+++ View/ViewDesigner.php
@@ -1,5 +1,5 @@
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 
@@ -9,129 +9,156 @@ include_once("ViewUser.php") ;
 class ViewDesigner extends ViewUser
 {
 
- public function Recentprojects($result)
- {
-   $html='';
-   $html.="<div class='recentprojects'>";
-   $html.="<h1>Recent Projects</h1>";
-   for ($i=0;$i < 3; $i++){
-     $row = $result->fetch_assoc();
-     $html.="<div class='image'>";
-     $html.="<img class='img2' src='images/SEO-illustrations-Project-02.png' alt='Avatar' height='150'>";
-     $html.="<h3 style='padding: 5px 66px;''>Project ".$row['id']."</h3>";
-     $html.="</div>";
-   }
-   return $html;
+  public function Recentprojects($result)
+  {
+    $html='';
+    $html.="<div class='recentprojects'>";
+    $html.="<h1>Recent Projects</h1>";
+    for ($i=0;$i < 3; $i++){
+      $row = $result->fetch_assoc();
+      $html.="<div class='image'>";
+      $html.="<img class='img2' src='images/SEO-illustrations-Project-02.png' alt='Avatar' height='150'>";
+      $html.="<h3 style='padding: 5px 66px;''>Project ".$row['id']."</h3>";
+      $html.="</div>";
+    }
+    return $html;
 
- }
-
- public function CreateProjectForm()
- {
-   $html='';
-   $html.="<form class='form' action='' method='post' enctype='multipart/form-data' name='ProjectForm'>";
-   $html.="<span class='form-title'>Create New Project</span>";
- $html.="<div class='create-form'>";
-   $html.="<label class='p-input'>Client Name</label>";
-   $html.="<input type='text' class='form-input' name='name' value='Project 1'>";
-
-   $html.="<label class='p-input'>Project Description</label>";
-   $html.="<input type='text' class='form-input' name='description' value='project 1 description here'>";
-
-   $html.="<label class='p-input'>Project Budget</label>";
-   $html.= "<input type='number' class='form-input' name='budget' value='32000 L.E'>";
-
-   $html.="<label class='p-input'>Images</label>";
-
-   $html.="<input type='file' class='form-file' id='imageupload' name='image[]' value='Upload'  onchange='getUploadedImages(this.id)' multiple>";
-   $html.="<div id='uploadedimages'>  </div>";
-
-   $html.="<label class='p-input'>Files</label>";
-   $html.="<input type='file' class='form-file' id='fileupload'name='file[]' value='Upload'onchange='getUploadedFiles(this.id)' multiple > <br>";
-   $html.="<div id='uploadedfiles'> </div>";
- $html.= "</div>";
-
-   $html.= "<input  type='submit' class='create' name='submit' value='Create'>";
-   $html.= "</form>";
-   return $html;
-
-
-
- }
- public function Projectslist($result)
- {
-   $online_name=unserialize($_SESSION['online'])->Name;
-   $online_id=unserialize($_SESSION['online'])->ID;
-   $html='';
-   if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-
-    $id= $row["id"];
-    $ClientName=$row["name"];
-    $images=$row["images"];
-    $firstimage = "uploads/projects/images/".explode(",", $images)[0];
-    $status=$row["status"];
-    $html.="<div class='project'>";
-    $html.= "<div class='p-img'>";
-    $html.="<img src='$firstimage' height='100%' alt='project image' >";
-    $html.="</div>";
-    $html.="<div class='p-info'>";
-    $html.="<h1>Project ID : $id</h1><br>";
-    $html.="<div class='p-h3'>Client : $ClientName</div><br>";
-    $html.="<div class='p-h3'>Designer 1: $online_name</div><br>";
-    $html.="<div class='p-h3'>Designer ID :$online_id</div>";
-    $html.="</div>";
-    $html.="<div class='my-p-status'>";
-    $html.="<h3>Status:<span class='status-orange'>$status</span></h3>";
-    $html.="</div>";
-    $html.="</div>";
   }
+
+  public function CreateProjectForm()
+  {
+    $html='';
+    $html.="<form class='form' action='' method='post' enctype='multipart/form-data' name='ProjectForm'>";
+    $html.="<span class='form-title'>Create New Project</span>";
+    $html.="<div class='create-form'>";
+    $html.="<label class='p-input'>Client Name</label>";
+    $html.="<input type='text' class='form-input' name='name' value='Project 1'>";
+
+    $html.="<label class='p-input'>Project Description</label>";
+    $html.="<input type='text' class='form-input' name='description' value='project 1 description here'>";
+
+    $html.="<label class='p-input'>Project Budget</label>";
+    $html.= "<input type='number' class='form-input' name='budget' value='32000 L.E'>";
+
+    $html.="<label class='p-input'>Images</label>";
+
+    $html.="<input type='file' class='form-file' id='imageupload' name='image[]' value='Upload'  onchange='getUploadedImages(this.id)' multiple>";
+    $html.="<div id='uploadedimages'>  </div>";
+
+    $html.="<label class='p-input'>Files</label>";
+    $html.="<input type='file' class='form-file' id='fileupload'name='file[]' value='Upload'onchange='getUploadedFiles(this.id)' multiple > <br>";
+    $html.="<div id='uploadedfiles'> </div>";
+    $html.= "</div>";
+
+    $html.= "<input  type='submit' class='create' name='submit' value='Create'>";
+    $html.= "</form>";
+    return $html;
+
+
+
   }
-  else {
-  echo "0 results";
-   }
+  public function Projectslist($result)
+  {
+    $online_name=unserialize($_SESSION['online'])->Name;
+    $online_id=unserialize($_SESSION['online'])->ID;
+    $html='';
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+
+        $id= $row["id"];
+        $ClientName=$row["name"];
+        $images=$row["images"];
+        $firstimage = "uploads/projects/images/".explode(",", $images)[0];
+        $status=$row["status"];
+        $html.="<div class='project'>";
+        $html.= "<div class='p-img'>";
+        $html.="<img src='$firstimage' height='100%' alt='project image' >";
+        $html.="</div>";
+        $html.="<div class='p-info'>";
+        $html.="<h1>Project ID : $id</h1><br>";
+        $html.="<div class='p-h3'>Client : $ClientName</div><br>";
+        $html.="<div class='p-h3'>Designer 1: $online_name</div><br>";
+        $html.="<div class='p-h3'>Designer ID :$online_id</div>";
+        $html.="</div>";
+        $html.="<div class='my-p-status'>";
+        $html.="<h3>Status:<span class='status-orange'>$status</span></h3>";
+        $html.="</div>";
+        $html.="</div>";
+      }
+    }
+    else {
+      echo "0 results";
+    }
+    return $html;
+  }
+
+  public function HomeMyProjects($result)
+  {
+    $html='';
+    if ($result->num_rows > 0) {
+        for($i=0;$i<4;$i++)
+        {
+          $row = $result->fetch_assoc();
+          $images=$row['images'];
+          $firstimage = "uploads/projects/images/".explode(",", $images)[0];
+          $id=$row['id'];
+          $html.="<div class='Myprojects'>";
+          $html.="<div class='image' id='color'>";
+          $html.="<img  src='$firstimage' alt='Project Image' height='150'>";
+          $html.="<h3 style='color:#76323F'>Project $id</h3>";
+          $html.="</div>";
+          $html.="</div>";
+        }
+
+      }
+
+    else {
+      echo "0 results";
+    }
    return $html;
+  }
+
 }
-}
- ?>
+?>
 <script type="text/javascript">
 var arr = new DataTransfer();
 var arr2=new DataTransfer();
 function getUploadedImages(id){
-    var numFiles = document.getElementById(id).files.length;
+  var numFiles = document.getElementById(id).files.length;
 
-    for(var i=0;i<numFiles;i++)
-    {
-      //var str=myFile.toString();
+  for(var i=0;i<numFiles;i++)
+  {
+    //var str=myFile.toString();
 
-      var filename = document.getElementById(id).files[i].name;
-      var div = document.getElementById('uploadedimages');
-      div.innerHTML += filename+','+'&nbsp &nbsp';
+    var filename = document.getElementById(id).files[i].name;
+    var div = document.getElementById('uploadedimages');
+    div.innerHTML += filename+','+'&nbsp &nbsp';
 
-    }
+  }
 
-    for (var y = 0; y < numFiles; y++) {
-      arr.items.add(document.getElementById(id).files[y]);
-    }
-    document.getElementById(id).files=arr.files;
+  for (var y = 0; y < numFiles; y++) {
+    arr.items.add(document.getElementById(id).files[y]);
+  }
+  document.getElementById(id).files=arr.files;
 
 }
 
 function getUploadedFiles(id){
-    var numFiles = document.getElementById(id).files.length;
-    for(var i=0;i<numFiles;i++)
-    {
+  var numFiles = document.getElementById(id).files.length;
+  for(var i=0;i<numFiles;i++)
+  {
 
-      var filename = document.getElementById(id).files[i].name;
-      var div = document.getElementById('uploadedfiles');
-      div.innerHTML += filename+','+'&nbsp &nbsp';
+    var filename = document.getElementById(id).files[i].name;
+    var div = document.getElementById('uploadedfiles');
+    div.innerHTML += filename+','+'&nbsp &nbsp';
 
-    }
+  }
 
-    for (var y = 0; y < numFiles; y++) {
-      arr2.items.add(document.getElementById(id).files[y]);
-    }
-    document.getElementById(id).files=arr2.files;
+  for (var y = 0; y < numFiles; y++) {
+    arr2.items.add(document.getElementById(id).files[y]);
+  }
+  document.getElementById(id).files=arr2.files;
 
 }
 
