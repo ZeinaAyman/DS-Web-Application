582477e45175b5423df1ac1a275f0a4748f231d1
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2021-06-18 23:57:13 +0200

    content diplay on user type

--- Home.php
+++ Home.php
@@ -29,7 +29,10 @@
   <body>
     <div class="flex-container">
       <div class="homecontainer">
-        <?php echo $viewDesigner->Recentprojects($UserController->C_GetProjects()); ?>
+        <?php if(unserialize($_SESSION['online'])->Type==1){ echo $viewDesigner->Recentprojects($UserController->C_GetProjects());}
+        else {
+          echo $viewDesigner->Recentprojects($UserController->C_GetAllProjects());
+        } ?>
 
         <div class="timeline">
             <h1 style="color: #76323F; padding: 20px; padding-bottom: 10px; margin-bottom: 0px;">Timeline</h1>
@@ -46,7 +49,7 @@
 
       <div class="color">
         <div class="homecontainer" style="width: 70%;">
-          <?php echo $viewDesigner->Myprojects($UserController->C_GetProjects()); ?>
+          <?php if(unserialize($_SESSION['online'])->Type==2){ echo $viewDesigner->Myprojects($UserController->C_GetProjects()); }?>
         </div>
       </div>
 
