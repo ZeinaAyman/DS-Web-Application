b0f939c3b5fd7d535b6369f8c0dc2e42bf6ddb35
Abdelrhman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-06-19 00:01:33 +0200

    Added ById function

--- AllProjects.php
+++ AllProjects.php
@@ -1,27 +1,15 @@
 <?php include 'navbar.php';
-      include 'DB.php';
-
-      $sql = "SELECT * FROM project";
-      $result = $conn->query($sql);
+      include 'View/ViewDesigner.php';
+      include 'Controller/DesignerController.php';
+      $viewDesigner = new ViewDesigner();
+      $Model = new Designer();
+      $DesignerController = new DesignerController($Model);
       ?>
 <link rel="stylesheet" href="Project.css">
+/*
 <body onload="ready()">
   <h1 class="title">All Projects</h1>
-  <?php while($row = $result->fetch_assoc()) { ?>
-  <div class="project"  >
-    <div class="p-img">
-
-    </div>
-    <div class="p-info">
-      <h1>Project: <?php echo $row['id']; ?></h1><br>
-      <div class="p-h3">Client: <?php echo $row['name']; ?></div><br>
-      <div class="p-h3">Designer 1 FullName</div><br>
-      <div class="p-h3">Designer 1 ID</div>
-    </div>
-  </div>
-
-<?php } ?>
-
+  <?php echo $viewDesigner->AllProjectsList($DesignerController->C_GetAllProjects()); ?>
 
 </body>
 <script type="text/javascript">
