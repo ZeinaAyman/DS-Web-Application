e1e6b4d0442b594f57408c3f18aa6073ef4aed91
zeina <zeina.ayman@hotmail.com>
2021-06-21 16:51:30 +0200

    admincontroller fix

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -28,7 +28,7 @@ class AdminController extends UserController
 
       if(empty($errors)==true)
       {
-        move_uploaded_file($file_tmp,"uploads/profilepictures/".$file_name);
+        move_uploaded_file($file_tmp,"../../public/uploads/profilepictures/".$file_name);
 
       }
 
